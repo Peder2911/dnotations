3a731c606c8c5c3726bb9b745c9be2cdd5c57552
Peder Landsverk <pglandsverk@gmail.com>
2024-03-10 18:28:59 +0100

    Rewrote, made the parsing of dbus output more elegant. Not working.

--- cmd/dnotations/main.go
+++ cmd/dnotations/main.go
@@ -1,4 +1,5 @@
-/* dnotations
+/*
+	dnotations
 
 This is a (wip) tool for people who would like to organize their Systemd units with metadata and annotations.
 This might be helpful to keep track of things like what services provide what functionality on a box, behind
@@ -21,9 +22,11 @@ Description=A proxy server.
 package main
 
 import (
+	"log"
+	"sync"
+
 	"github.com/go-yaml/yaml"
 	"github.com/godbus/dbus/v5"
-	"log"
 
 	//"github.com/go-yaml/yaml"
 	"context"
@@ -45,64 +48,95 @@ type UnitStatus struct {
 	JobPath     dbus.ObjectPath // The job object path
 }
 
-type UnitMetadata struct {
-	Annotations map[string]string `yaml:"annotations"`
-}
-
-func (u UnitMetadata) String() string {
-	kv := make([]string, len(u.Annotations))
-	var i int
-	for k,v := range u.Annotations{
-		kv[i] = fmt.Sprintf("%s:\t%s",k,v)
-		i++
-	}
-	return "Metadata:"+strings.Join(kv,",")+"\n"
-}
-
-type Unit struct {
-	Name string
-	Metadata UnitMetadata
-}
-
-type Units []Unit
-
 func (u UnitStatus) String() string {
 	return u.Name
 }
 
-func ListUnits(ctx context.Context, conn *dbus.Conn)(*[]UnitStatus, error){
-	systemd := conn.Object("org.freedesktop.systemd1","/org/freedesktop/systemd1")
-	var result [][]interface{}
-	err := systemd.CallWithContext(ctx,"org.freedesktop.systemd1.Manager.ListUnits", 0).Store(&result)
-	if err != nil {
-		return nil, err
-	}
-	result_interface  := make([]interface{}, len(result))
-	for i := range result {
-		result_interface[i] = result[i]
-	}
-
-	unit_status := make([]UnitStatus, len(result))
-	unit_status_interface := make([]interface{}, len(unit_status))
-	for i := range unit_status{ 
-		unit_status_interface[i] = &unit_status[i]
-	}
-	err = dbus.Store(result_interface, unit_status_interface...)
-	if err != nil {
-		return nil, err
-	}
-	return &unit_status, nil
+type UnitFile struct {
+	Path   string
+	Status string
 }
 
-func ParseUnitHeader(data []byte) (UnitMetadata, error) {
+func (u UnitFile) String() string{
+	return fmt.Sprintf("Path: %s\tStatus %s", u.Path, u.Status)
+}
+
+type DnotatedSystemd struct {
+	Conn *dbus.Conn
+}
+
+func (s *DnotatedSystemd) object() dbus.BusObject {
+	return s.Conn.Object("org.freedesktop.systemd1","/org/freedesktop/systemd1")
+}
+
+func (s *DnotatedSystemd) mancall(ctx context.Context,method string, retvalues *[][]interface{}) error {
+	obj := s.object()
+	call := obj.CallWithContext(ctx, fmt.Sprintf("org.freedesktop.systemd1.Manager.%s",method),0)
+	err := call.Store(retvalues)
+	return err
+}
+
+// func (s *DnotatedSystemd) ListUnits(ctx context.Context)(*[]UnitStatus, error){
+// 	var result[][]interface{}
+// 	err := s.mancall(ctx, "ListUnits",&result)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	units := make([]UnitStatus, len(result))
+// 	for i,r := range result {
+// 		units[i] = UnitStatus{
+// 			r[0].(string),
+// 			r[1].(string),
+// 			r[2].(string),
+// 			r[3].(string),
+// 			r[4].(string),
+// 			r[5].(string),
+// 			r[6].(dbus.ObjectPath),
+// 			r[7].(uint32),
+// 			r[8].(string),
+// 			r[9].(dbus.ObjectPath),
+// 		}
+// 	}
+// 	return &units,err
+// }
+
+func (s *DnotatedSystemd) listUnitFiles(ctx context.Context)(*[]UnitFile, error){
+	var result[][]interface{}
+	err := s.mancall(ctx, "ListUnitFiles",&result)
+	if err != nil {
+		return nil, err
+	}
+	units := make([]UnitFile, len(result))
+	for i,r := range result {
+		units[i] = UnitFile{
+			r[0].(string),
+			r[1].(string),
+		}
+	}
+	return &units,err
+}
+
+type DnotatedUnitHeader struct {
+	Annotations DnotatedUnit `yaml:"annotations"`
+}
+
+func ParseUnitHeader(data []byte) (DnotatedUnitHeader, error) {
 	var yaml_header_lines []string
+	var header DnotatedUnitHeader
 	lines := strings.Split(string(data),"\n")
 
 	var in_header bool = true
 	var i int = 0
 	var line string
+	if len(lines) == 0 {
+		return header,fmt.Errorf("Unit file had no header.")
+	}
 	for in_header {
 		line = lines[i]
+		if len(line) == 0 {
+			continue
+		}
+
 		if line[0] == '#' {
 			yaml_header_lines = append(yaml_header_lines, strings.TrimLeft(line, "#") )
 		} else {
@@ -110,57 +144,81 @@ func ParseUnitHeader(data []byte) (UnitMetadata, error) {
 		}
 		i++
 	}
-	var unit_metadata UnitMetadata
-	yaml.Unmarshal([]byte(strings.Join(yaml_header_lines, "\n")), &unit_metadata)
-	return unit_metadata, nil
-
+	yaml.Unmarshal([]byte(strings.Join(yaml_header_lines, "\n")), &header)
+	return header, nil
 }
 
-func LoadUnit(name string) (*Unit,error) {
-	data,err := os.ReadFile(fmt.Sprintf("/etc/systemd/system/%s",name))
+type DnotatedUnit struct {
+	PartOf    string `yaml:"part-of"`
+	Component string `yaml:"component"`
+	Version   string `yaml:"version"`
+	ManagedBy string `yaml:"managed-by"`
+	Hostname  string `yaml:"hostname"`
+}
+
+func (u DnotatedUnit) String() string {
+	return fmt.Sprintf(`
+	Part of:\t%s
+	Component:\t%s
+	Version:\t%s
+	Managed by:\t%s
+	Hostname:\t%s
+	---
+	`, u.PartOf,u.Component,u.Version,u.ManagedBy,u.Hostname)
+}
+
+func LoadDnotatedUnit(path string) (DnotatedUnit, error){
+	var unit DnotatedUnit
+	data,err := os.ReadFile(path)
+	if err != nil {
+		return unit, err
+	}
+	header,err := ParseUnitHeader(data)
+	unit = header.Annotations
+	if err != nil {
+		return unit,err
+	}
+	return unit,nil
+}
+
+func (s *DnotatedSystemd) ListUnits(ctx context.Context) (*[]DnotatedUnit, error) {
+	files,err := s.listUnitFiles(ctx)
 	if err != nil {
 		return nil,err
 	}
-
-	unit_header,err := ParseUnitHeader(data)
-	if err != nil {
-		log.Println(fmt.Sprintf("Failed to parse header for unit %s: %s",name,err))
-	}
-
-	var unit Unit
-	unit.Name = name
-	unit.Metadata = unit_header
-	return &unit,nil
-}
-
-func LoadUnits() (Units,error) {
-	entries, err := os.ReadDir("/etc/systemd/system")
-	if err != nil {
-		return nil, err
-	}
-	var units Units
-	for _,e := range entries {
-		if e.Type().IsRegular() {
-			unit,err := LoadUnit(e.Name())
+	var units []DnotatedUnit = make([]DnotatedUnit, len(*files))
+	var wg sync.WaitGroup
+	for i,f := range *files {
+		wg.Add(1)
+		go func(file UnitFile, units *[]DnotatedUnit, index int){
+			defer wg.Done()
+			fmt.Printf("Doing %s\n",file.Path)
+			unit, err := LoadDnotatedUnit(file.Path)
+			fmt.Printf("Loaded unit %s\n",file.Path)
 			if err != nil {
-				return nil, err
+				log.Printf("Failed to load unit at %s: %s\n", file.Path, err)
+				return
 			}
-			units = append(units, *unit)
-		}
-	} 
-	return units, nil
+			(*units)[index] = unit
+			fmt.Printf("Done with %s\n",file.Path)
+		}(f,&units,i)
+	}
+	wg.Wait()
+	return &units,nil
 }
 
 func main() {
-	units,err := LoadUnits()
+	ctx := context.Background()
+	conn,err := dbus.ConnectSystemBus()
+	if err != nil {panic(err)}
+	defer conn.Close()
+	s := DnotatedSystemd{conn}
+
+	units,err := s.ListUnits(ctx)
 	if err != nil {
 		panic(err)
 	}
-	for _,unit := range units {
-		println(unit.Name)
-		println("\tannotations:")
-		for k,v := range unit.Metadata.Annotations {
-			println(fmt.Sprintf("\t\t%s:\t%s", k,v))
-		}
+	for _,unit := range *units {
+		fmt.Printf("%s\n",unit)
 	}
 }
